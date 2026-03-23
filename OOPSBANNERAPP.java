9525bce2381dde938c7a3ff5332b43a285cb37d9
Himanshu garg <himanshugarg@Himanshus-MacBook-Air-9.local>
2026-03-23 09:41:02 +0530

    UC4 - implemented array and loop for banner

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,5 +1,22 @@
-class OOPSBANNERAPP{
-    public static void main(String [] args){
-        System.out.println("OOPS");
+class OOPSBANNERAPP {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("   ", "*****", "*****", "*****", "*****"),
+            String.join("   ", "*   *", "*   *", "*   *", "*   *"),
+            String.join("   ", "*   *", "*   *", "*   *", "*   *"),
+            String.join("   ", "*   *", "*****", "*   *", "*****"),
+            String.join("   ", "*   *", "*    ", "*   *", "    *"),
+            String.join("   ", "*   *", "*    ", "*   *", "    *"),
+            String.join("   ", "*****", "*    ", "*****", "*****")
+        };
+
+        // Loop through array
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
+
+
