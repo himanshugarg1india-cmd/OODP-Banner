7d5c6635188523d7d3e4d13997bad55d18018d6f
Himanshu garg <himanshugarg@Himanshus-MacBook-Air-9.local>
2026-03-23 11:23:11 +0530

    UC8 - Map Collection implementation

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,5 +1,55 @@
-class OOPSBANNERAPP{
-    public static void main(String [] args){
-        System.out.println("OOPS");
+import java.util.*;
+
+class OOPSBANNERAPP {
+
+    // Map to store character patterns
+    static Map<Character, String[]> patterns = new HashMap<>();
+
+    // Initialize patterns
+    static void initializePatterns() {
+        patterns.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patterns.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patterns.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+    }
+
+    // Function to print banner
+    static void printBanner(String word) {
+        for (int i = 0; i < 7; i++) {
+            for (char ch : word.toCharArray()) {
+                System.out.print(patterns.get(ch)[i] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        initializePatterns();
+        printBanner("OOPS");
     }
 }
