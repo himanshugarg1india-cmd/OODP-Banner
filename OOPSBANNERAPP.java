b0c098dc81547e126f964ec149833ac4e1829d8a
Himanshu garg <himanshugarg@Himanshus-MacBook-Air-9.local>
2026-03-23 11:20:39 +0530

    UC7 - Inner Class implementation

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,5 +1,62 @@
-class OOPSBANNERAPP{
-    public static void main(String [] args){
-        System.out.println("OOPS");
+class OOPSBANNERAPP {
+
+    // Inner Class
+    static class CharacterPattern {
+        char character;
+        String[] pattern;
+
+        CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPattern[] letters = {
+
+            new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+            }),
+
+            new CharacterPattern('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+            }),
+
+            new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+            })
+        };
+
+        String word = "OOPS";
+
+        for (int i = 0; i < 7; i++) {
+            for (char ch : word.toCharArray()) {
+                for (CharacterPattern cp : letters) {
+                    if (cp.character == ch) {
+                        System.out.print(cp.pattern[i] + "  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
