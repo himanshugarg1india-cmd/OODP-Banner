c210bdd81937c1ff0255271d47c74298b0b5a1cf
Himanshu garg <himanshugarg@Himanshus-MacBook-Air-9.local>
2026-03-23 11:17:52 +0530

    UC6 - implemented static methods for banner (DRY principle)

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,5 +1,53 @@
-class OOPSBANNERAPP{
-    public static void main(String [] args){
-        System.out.println("OOPS");
+class OOPSBANNERAPP {
+
+    public static void main(String[] args) {
+
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + "   " + o[i] + "   " + p[i] + "   " + s[i]);
+        }
+    }
+
+    // Method for O
+    static String[] getO() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
+    }
+
+    // Method for P
+    static String[] getP() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*****",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method for S
+    static String[] getS() {
+        return new String[] {
+            "*****",
+            "*    ",
+            "*    ",
+            "*****",
+            "    *",
+            "    *",
+            "*****"
+        };
     }
 }
+    
